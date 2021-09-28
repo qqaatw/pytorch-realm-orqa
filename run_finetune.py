86c61f55d3c51b0fba62b8a4be1b03ff4c49eec8
qqaatw <qqaatw@gmail.com>
2021-09-29 00:00:58 +0800

    Add eval metrics

--- run_finetune.py
+++ run_finetune.py
@@ -1,12 +1,13 @@
 from argparse import ArgumentParser
 import os
+import re
 import logging
+import string
+import unicodedata
 
 
 import torch
 import itertools
-from datasets import load_dataset
-from datasets.utils import DownloadConfig
 from transformers import RealmConfig, get_linear_schedule_with_warmup
 from transformers.optimization import AdamW
 from transformers.models.realm.modeling_realm import logger as model_logger
@@ -27,13 +28,63 @@ class DataCollator(object):
     def __call__(self, batch):
         example = batch[0]
         question = example["question.text"]
-        answer_text = example["annotations.short_answers"][0]["text"]
-        if len(answer_text) != 0:
-            answers = self.tokenizer(answer_text, add_special_tokens=False, return_token_type_ids=False, return_attention_mask=False).input_ids
+        answer_texts = example["annotations.short_answers"][0]["text"]
+        if len(answer_texts) != 0:
+            answers = self.tokenizer(answer_texts, add_special_tokens=False, return_token_type_ids=False, return_attention_mask=False).input_ids
         else:
             answers = [[-1]]
-        return question, answers
+        return question, answer_texts, answers
+
+def compute_eval_metrics(labels, predicted_answer, reader_output):
+    """Compute eval metrics."""
+    def normalize_answer(s):
+        """Normalize answer. (Directly copied from ORQA codebase)"""
+        s = unicodedata.normalize("NFD", s)
+
+        def remove_articles(text):
+            return re.sub(r"\b(a|an|the)\b", " ", text)
+
+        def white_space_fix(text):
+            return " ".join(text.split())
+
+        def remove_punc(text):
+            exclude = set(string.punctuation)
+            return "".join(ch for ch in text if ch not in exclude)
+
+        def lower(text):
+            return text.lower()
+
+        return white_space_fix(remove_articles(remove_punc(lower(s))))
     
+    # []
+    exact_match = torch.index_select(
+        torch.index_select(
+            reader_output.reader_correct,
+            dim=0,
+            index=reader_output.block_idx
+        ),
+        dim=1,
+        index=reader_output.candidate
+    )
+
+    def _official_exact_match(predicted_answer, references):
+        return max(
+            [normalize_answer(predicted_answer) == normalize_answer(reference) for reference in references]
+        )
+
+    official_exact_match = _official_exact_match(predicted_answer, labels)
+
+    # TODO: Batch?
+    eval_metric = dict(
+        exact_match=torch.mean(exact_match),
+        official_exact_match=torch.mean(official_exact_match),
+        reader_oracle=torch.mean(torch.any(reader_output.reader_correct))
+    )
+    
+    for k in (5, 10, 50, 100, 500, 1000, 5000):
+        eval_metric["top_{}_match".format(k)] = torch.mean(
+            torch.any(reader_output.retriever_correct[:k]))
+    return eval_metric
 
 def retrieve(args, searcher, tokenizer, question):
     question_ids = tokenizer([question], return_tensors='pt')
@@ -131,7 +182,30 @@ def main(args):
 
     searcher, reader, tokenizer = get_searcher_reader_tokenizer(args, config)
 
+    optimizer = AdamW(
+        itertools.chain(searcher.parameters(), reader.parameters()),
+        lr=args.learning_rate,
+        weight_decay=0.01,
+    )
+    lr_scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=min(10000, max(100,
+                                    int(args.num_training_steps / 10))),
+        num_training_steps=args.num_training_steps,
+    )
+
+    data_collector = DataCollator(tokenizer)
+
     if args.is_train:
+        if args.resume:
+            checkpoint = torch.load(os.path.join(args.model_dir, "checkpoint.bin"))
+            searcher.load_state_dict(checkpoint["searcher_state_dict"])
+            reader.load_state_dict(checkpoint["reader_state_dict"])
+            optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+            global_step = checkpoint["training_step"]
+        else:
+            global_step = 1
+
         # Setup training mode
         searcher.train()
         searcher.to(args.device)
@@ -141,7 +215,6 @@ def main(args):
 
         # Setup data
         print(training_dataset)
-        data_collector = DataCollator(tokenizer)
         train_dataloader = torch.utils.data.DataLoader(
             dataset=training_dataset,
             batch_size=1,
@@ -149,23 +222,9 @@ def main(args):
             collate_fn=data_collector
         )
 
-        optimizer = AdamW(
-            itertools.chain(searcher.parameters(), reader.parameters()),
-            lr=args.learning_rate,
-            weight_decay=0.01,
-        )
-        lr_scheduler = get_linear_schedule_with_warmup(
-            optimizer=optimizer,
-            num_warmup_steps=min(10000, max(100,
-                                        int(args.epochs * len(train_dataloader) / 10))),
-            num_training_steps=args.epochs * len(train_dataloader),
-        )
-
-        global_step = 0
-
-        for epoch in range(1, args.epochs + 1):
+        while True:
             for batch in train_dataloader:
-                question, answers = batch
+                question, answer_texts, answers = batch
                 retriever_output = retrieve(args, searcher, tokenizer, question)
                 reader_output, predicted_answer = read(args, reader, tokenizer, retriever_output, question, answers)
 
@@ -173,28 +232,47 @@ def main(args):
                 optimizer.step()
                 lr_scheduler.step()
 
-                logging.info(f"Step: {optimizer._step_count} Retriever Loss: {reader_output.retriever_loss.mean()} Reader Loss: {reader_output.reader_loss.mean()}\nQuestion: {question} Gold Answer: {tokenizer.batch_decode(answers) if answers != [[-1]] else None} Predicted Answer: {predicted_answer}")
-        
-        if epoch % args.chpt_interval == 0:
-            torch.save(
-                {
-                    'searcher_state_dict': searcher.state_dict(),
-                    'reader_state_dict': reader.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                }, 
-                os.path.join(args.model_dir, "checkpoint.bin")
-            )
+                logging.info(f"Step: {global_step} Retriever Loss: {reader_output.retriever_loss.mean()} Reader Loss: {reader_output.reader_loss.mean()}\nQuestion: {question} Gold Answer: {tokenizer.batch_decode(answers) if answers != [[-1]] else None} Predicted Answer: {predicted_answer}")
+
+                if global_step % args.ckpt_interval == 0:
+                    metrics = compute_eval_metrics(answer_texts, predicted_answer, reader_output)
+                    logging.info(f"Step: {global_step} {metrics}")
+                    logging.info(f"Saving checkpint at step {global_step}")
+                    torch.save(
+                        {
+                            'searcher_state_dict': searcher.state_dict(),
+                            'reader_state_dict': reader.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                            'training_step': global_step
+                        }, 
+                        os.path.join(args.model_dir, "checkpoint.pt")
+                    )
+
+                global_step += 1
+                if global_step >= args.num_training_steps:
+                    break
+            if global_step >= args.num_training_steps:
+                break
 
     else:
         searcher.eval()
         reader.eval()
-        data_collector = DataCollator(tokenizer)
+
+        # Setup data
+        print(eval_dataset)
         eval_dataloader = torch.utils.data.DataLoader(
             dataset=eval_dataset,
             batch_size=1,
-            shuffle=True,
+            shuffle=False,
             collate_fn=data_collector
         )
+        for batch in eval_dataloader:
+            question, answers = batch
+            with torch.no_grad():
+                retriever_output = retrieve(args, searcher, tokenizer, question)
+                reader_output, predicted_answer = read(args, reader, tokenizer, retriever_output, question, answers)
+            metrics = compute_eval_metrics(answers, predicted_answer, reader_output)
+            logging.info(f"{metrics}")
         
 
 
@@ -211,9 +289,9 @@ if __name__ == "__main__":
     # Training hparams
     parser.add_argument("--device", type=str, default='cuda')
     parser.add_argument("--learning_rate", type=float, default=1e-5)
-    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--num_training_steps", type=int, default=100)
     parser.add_argument("--is_train", action="store_true")
-    parser.add_argument("--num_workers", type=int, default=10)
+    parser.add_argument("--resume", action="store_true")
     parser.add_argument("--ckpt_interval", type=int, default=10)
 
     # Retriever
@@ -221,7 +299,7 @@ if __name__ == "__main__":
     parser.add_argument("--block_records_path", type=str, default=r"./data/enwiki-20181220/blocks.tfr")
     parser.add_argument("--retriever_path", type=str, default=r"./data/cc_news_pretrained/embedder/variables/variables")
     # Reader
-    parser.add_argument("--checkpoint_path", type=str, default=r"./data/orqa_nq_model_from_realm/export/best_default/checkpoint/model.ckpt-300000")
+    parser.add_argument("--checkpoint_path", type=str, default=r"./data/cc_news_pretrained/bert/variables/variables")
 
     args = parser.parse_args()
     
