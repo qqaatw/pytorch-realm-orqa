4581d022c25ebcc04a698bdfb29cf0b934ebd267
qqaatw <qqaatw@gmail.com>
2022-02-03 18:36:09 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,16 +1,23 @@
-import os
 import unittest
-
+import torch
 import predictor, run_finetune
 
 
 class Tester(unittest.TestCase):
     def setUp(self):
-        self.pt_checkpoint_path = "google/realm-orqa-nq-openqa"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.pt_checkpoint_pretrained_name = "./export_nq_newqa/realm-orqa-nq-openqa"
+        
+        self.model_dir = "./"
+        self.pt_checkpoint_name = "checkpoint"
+        self.pt_checkpoint_step = 158330
 
     def test_predictor(self):
         parser = predictor.get_arg_parser()
-        args = parser.parse_args(["--question", "Who is the pioneer in modern computer science?"])
+        args = parser.parse_args([
+            "--question", "Who is the pioneer in modern computer science?",
+            "--checkpoint_pretrained_name", self.pt_checkpoint_pretrained_name,
+        ])
         answer = predictor.main(args)
 
         self.assertEqual(answer, "alan mathison turing")
@@ -19,7 +26,8 @@ class Tester(unittest.TestCase):
         parser = predictor.get_arg_parser()
         args = parser.parse_args([
             "--question", "What is the previous name of Meta Platform, Inc.?",
-            "--additional_documents_path", "additional_documents.npy"
+            "--checkpoint_pretrained_name", self.pt_checkpoint_pretrained_name,
+            "--additional_documents_path", "additional_documents.npy",
         ])
         answer = predictor.main(args)
 
@@ -32,8 +40,11 @@ class Tester(unittest.TestCase):
         run_finetune.main(args)
 
     def test_finetune_eval(self):
-        return
         parser = run_finetune.get_arg_parser()
         args = parser.parse_args([
-            "--checkpoint_pretrained_name", self.pt_checkpoint_path,])
+            "--model_dir", self.model_dir,
+            "--checkpoint_name", self.pt_checkpoint_name,
+            "--checkpoint_step", str(self.pt_checkpoint_step),
+            "--device", self.device,
+        ])
         run_finetune.main(args)
